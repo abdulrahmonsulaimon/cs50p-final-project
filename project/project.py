7ddc69362600720f6fe291d2db77846725ce1d8a
Abdulrahmon <abdulrahmonsulaimon60@gmail.com>
2023-11-12 00:27:32 +0100

    Implemented audio to text and read text

--- project/project.py
+++ project/project.py
@@ -1,35 +1,82 @@
 import tkinter as tk
 from halo import Halo
-import time
+from os import path
 import wave
 import sys
 import pyaudio
 import threading
+import speech_recognition as sr
+import pyttsx3
+
 
 stopped = False
+
 class StopError(Exception):
     def __init__(self, message="Stopped!"):
         self.message = message
         super().__init__(self.message)
 
+
+
+def wav_to_txt(wav_path=None):
+    if wav_path == None:
+        audio_file = path.join(path.dirname(path.realpath(__file__)), "output.wav")
+
+    r = sr.Recognizer()
+    with sr.AudioFile(audio_file) as source:
+        audio = r.record(source)
+
+    try:
+        text = r.recognize_google(audio)
+        with open("output.txt", "w") as file:
+            file.write(text)
+              
+    except sr.UnknownValueError:
+        print("Could not understand the audio")
+    except sr.RequestError as e:
+        print(f"Request Error!: {e}")
+
+
+
+def txt_to_pdf(txt_path=None):
+    ...
+
+
+def read_txt(text=None, txt_path="output.txt"):
+
+    with open(txt_path, "r") as file:
+        text = file.readlines()
+    
+    for line in text:
+        print(line)  
+        engine = pyttsx3.init()
+        engine.say(line)
+        engine.runAndWait()
+
+
 # ++ spinner ++
 spinner = Halo(text="Recording...", spinner="dots")
+
 
 def show_spinner():
     spinner.start()
 
+
 def hide_spinner():
     spinner.stop()
+
+
 # ++ spinner ++
+
 
 # === voice recording ===
 def start_voice_recording(output_wav_file="output.wav", RECORD_SECONDS=360):
     CHUNK = 1024
     FORMAT = pyaudio.paInt16
-    CHANNELS = 1 if sys.platform == 'darwin' else 2
+    CHANNELS = 1 if sys.platform == "darwin" else 2
     RATE = 44100
 
-    with wave.open(output_wav_file, 'wb') as wf:
+    with wave.open(output_wav_file, "wb") as wf:
         p = pyaudio.PyAudio()
         wf.setnchannels(CHANNELS)
         wf.setsampwidth(p.get_sample_size(FORMAT))
@@ -49,8 +96,9 @@ def start_voice_recording(output_wav_file="output.wav", RECORD_SECONDS=360):
         stream.stop_stream()
         stream.close()
         p.terminate()
-
+        wav_to_txt()
 # === voice recording ===
+
 
 def record():
     global stopped
@@ -58,12 +106,17 @@ def record():
     show_spinner()
     threading.Thread(target=start_voice_recording).start()
 
+
 def stop_record():
     global stopped
     stopped = True
     hide_spinner()
     sys.exit("Done!")
 
+
+
+
+# <=== UI ===>
 def main():
     window = tk.Tk()
     window.title("AudiSpeech")
@@ -84,7 +137,7 @@ def main():
         padx=100,
         pady=20,
         font=("Helvetica", 25, "bold"),
-        command=record
+        command=record,
     )
 
     stop_record_button = tk.Button(
@@ -96,13 +149,27 @@ def main():
         padx=20,
         pady=15,
         font=("Helvetica", 18, "bold"),
-        command=stop_record
+        command=stop_record,
     )
 
+    read_txt_button = tk.Button(
+        window,
+        text="Read Text...",
+        bg="grey",
+        fg="white",
+        activebackground="grey",
+        padx=20,
+        pady=15,
+        font=("Helvetica", 18, "bold"),
+        command=read_txt,
+    )
     record_button.pack(pady=10)
     stop_record_button.pack(pady=10)
+    read_txt_button.pack(pady=10)
 
     window.mainloop()
+# <=== UI ===>
+
 
 if __name__ == "__main__":
     main()
